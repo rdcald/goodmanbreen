33fa74fb62c1839eaa2b28cbc2f13df92b3d03bb
Ron <rdcaldwell5705@eagle.fgcu.edu>
2016-06-06 16:25:25 -0400

    webpage
    preview

---
+++ assets/javascripts/index.js
@@ -0,0 +1,35 @@
+/* Controls navbar transparency */
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > 550) {
+        $(".navbar").addClass("not-transparent");
+        $(".navItems").removeClass("transparent");
+    }
+    else {
+        $(".navbar").removeClass("not-transparent");
+        $(".navItems").addClass("transparent");
+    }
+});
+
+/* Controls scroll */
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+				  scrollTop: target.offset().top
+				}, 1000);
+				return false;
+            }
+        }
+    });
+});
+
+/* Scrollspy targets */
+$('body').scrollspy({ target: '#services' });
+$('body').scrollspy({ target: '#contact' });
+$('body').scrollspy({ target: '#about' });
+
+/* Parallax window image */
+$('.parallax-window').parallax({imageSrc: 'file:///Users/roncaldwell/Documents/Developer/GoodmanBreen/assets/images/family.jpg'});
